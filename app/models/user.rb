21e88f792982e877e871b9ab576cc74a914d9f9f
vumaihoang <vumh@rubyonring.com>
2022-05-05 17:33:18 +0700

    update comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-  enum role: { admin: 0, user: 1 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}
   validates :name, presence:true
   validates :email, uniqueness: true
+
+  enum role: { admin: 0, user: 1 }
 end
